2182b1d737484f94a1250670b1264dd63af6ee1d
Kaymone1 <kennya.benoit15@gmail.com>
2023-11-18 11:40:54 -0500

    updated freezed packages

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos.apps.TodosConfig',
     'rest_framework',
+    'todos.apps.TodosConfig',
     'corsheaders'
 ]
 
